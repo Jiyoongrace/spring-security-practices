313081084109997487c9d02aa5a9810e7b1214e3
Jiyoongrace <giyun3632@naver.com>
2024-06-21 11:02:20 +0900

    :white_check_mark: feat: SecurityBuilder & SecurityConfigurer

--- ss-ex04/src/test/java/ex04/config/SecurityConfig05Test.java
+++ ss-ex04/src/test/java/ex04/config/SecurityConfig05Test.java
@@ -1,5 +1,6 @@
 package ex04.config;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -18,6 +20,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.DelegatingFilterProxy;
+
+import java.util.List;
 
 
 @ExtendWith(SpringExtension.class)
@@ -36,6 +40,25 @@ public class SecurityConfig05Test {
                 .webAppContextSetup(context)
                 .addFilter(new DelegatingFilterProxy(filterChainProxy), "/*")
                 .build();
+    }
+
+    @Test
+    public void testSecurityFilterChains() {
+        List<SecurityFilterChain> SecurityFilterChains = filterChainProxy.getFilterChains();
+        assertEquals(2, SecurityFilterChains.size());
+    }
+
+    @Test
+    public void testSecurityFilters() {
+        SecurityFilterChain securityFilterChain = filterChainProxy.getFilterChains().get(1);
+        List<Filter> filters =  securityFilterChain.getFilters();
+
+        assertEquals(13, filters.size());
+
+        // All Filters
+        for(Filter filter : filters) {
+            System.out.println(filter.getClass());
+        }
     }
 
     @Test
